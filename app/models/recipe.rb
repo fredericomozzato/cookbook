4d517dcf29308f486705d63de61022a652c4c339
Frederico <frederico.smozzato@gmail.com>
2023-12-13 15:26:00 -0300

    Implementa exibição da lista

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :recipe_type
   belongs_to :user
+  has_many :list_items
   validates :title, :cook_time, :ingredients, :instructions, presence: true
 end
--- app/models/recipe_list.rb
+++ app/models/recipe_list.rb
@@ -1,4 +1,5 @@
 class RecipeList < ApplicationRecord
   belongs_to :user
-  has_many :recipes, through: :list_item
+  has_many :list_items
+  has_many :recipes, through: :list_items
 end
